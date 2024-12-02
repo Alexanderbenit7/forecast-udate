113295e6c9e5ddfcad31fb58ac63a3a18fac5a35
Alexander Benites <abenitesalvarado@gmail.com>
2024-12-02 13:15:21 +0000

    commit

--- data-preprocessing.R
+++ data-preprocessing.R
@@ -7,8 +7,67 @@
 # --------------------------------------------------------------
 
 library(rio)
-
+library(dplyr)
+library(stringr)
 
 # -------------------------Import data--------------------------
 # --------------------------------------------------------------
+
+stores = import("/Users/alexander/Documents/MSc Data Science/Understanding Data and their Environment/Assignment 2/store.csv")
+trainData = import("/Users/alexander/Documents/MSc Data Science/Understanding Data and their Environment/Assignment 2/train.csv")
+testData = import("/Users/alexander/Documents/MSc Data Science/Understanding Data and their Environment/Assignment 2/test.csv")
+
+# -----------------------Irregularities-------------------------
+# --------------------------------------------------------------
+
+stores = stores %>% # For PromoInterval
+  mutate(across(everything(), ~ ifelse(str_trim(.) == "", NA, .)))
+
+
+
+# -------------------Missing data and imputation----------------
+# --------------------------------------------------------------
+
+# Cols with problems: CompetitionDistance, CompetitionOpenSinceMonth
+# CompetitionOpenSinceYear, Promo2SinceWeek, Promo2SinceYear
+
+sum(is.na(stores$CompetitionDistance)) # 3
+sum(is.na(stores$CompetitionOpenSinceMonth)) # 354
+sum(is.na(stores$CompetitionOpenSinceYear)) # 354
+sum(is.na(stores$Promo2SinceWeek)) # 508031
+sum(is.na(stores$Promo2SinceYear)) # 508031
+
+# Add a very large number that suggests absence of competition:
+stores$HasCompetition = ifelse(is.na(stores$CompetitionDistance), 0, 1) # Dummy for competition
+stores$CompetitionDistance[is.na(stores$CompetitionDistance)] = 80000
+
+
+
+
+# -------------------------Data integration---------------------
+# --------------------------------------------------------------
+
+# Check if we have unique stores in stores.csv
+unique_stores = stores %>% 
+  summarise(total_stores = n(),
+            unique_stores = n_distinct(Store))
+
+print(unique_stores) # We do: 1115 unique stores
+
+# Data unification
+data = merge(trainData, stores, by = "Store", all = TRUE)
+print(dim(data))
+
+
+# --------------- Data aggregation and visualization------------
+# --------------------------------------------------------------
+
+
+# -----------------------Data transformation--------------------
+# --------------------------------------------------------------
+
+# -----------------------Feature selection--------------------
+# --------------------------------------------------------------
+
+
 
